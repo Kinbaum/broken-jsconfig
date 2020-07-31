421591a965bc5548693de7703e184ee058a988a9
Kinbaum <matthew_kinbaum@optum.com>
2020-07-31 15:28:00 -0400

    build(init): showcasing error with monorepo and wp5

---
+++ packages/next-app/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header({ styles }) {
+    return (
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+    )
+}
